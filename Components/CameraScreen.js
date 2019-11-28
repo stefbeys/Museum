5b57029e6cd6568847b5404814365b42fe4934aa
beysinvaders <stef.beys@gmail.com>
2019-11-29 01:32:32 +0200

    Converted Image to base64 and added fetch command (not tested for results because: too late)

--- Components/CameraScreen.js
+++ Components/CameraScreen.js
@@ -1,86 +1,107 @@
 import React from "react";
-import { StyleSheet, View, Dimensions,Text,TouchableOpacity } from 'react-native';
-import * as Permissions from 'expo-permissions';
-import { Camera } from 'expo-camera';
+import {
+  StyleSheet,
+  View,
+  Dimensions,
+  Text,
+  TouchableOpacity
+} from "react-native";
+import * as Permissions from "expo-permissions";
+import { Camera } from "expo-camera";
 import { TouchableWithoutFeedback } from "react-native-gesture-handler";
-import ImgToBase64 from 'react-native-image-base64';
-import * as Filesystem from 'expo-file-system';
+import * as Filesystem from "expo-file-system";
 const ScreenHeight = Dimensions.get("window").height + 82;
 const ScreenWidth = Dimensions.get("window").width;
-const ENDPOINT="http://192.168.0.176:8080/api/";
+const ENDPOINT = "http://192.168.0.176:8080/api/";
 let _camera;
 export default class CameraScreen extends React.Component {
-    constructor(props){
-      super(props)
-      this.ScanImage=this.ScanImage.bind(this);
-    }
-    
-    state={
-        hasCameraPermission:null,
-        type: Camera.Constants.Type.back,
-    }
-    
-    ScanImage(){
-      console.log(_camera.takePictureAsync);
-      _camera.takePictureAsync().then(r=>{
-        console.log(r);
-        Filesystem.readAsStringAsync(r).then(s=>{
-          console.log(s.toString());
-        });
-        /*Filesystem.readAsStringAsync(r,{encoding:FileSystem.EncodingType.Base64}).then(s=>{
-          console.log(s);
-        })*/});
-    }
-    async componentDidMount() {
-        const { status } = await Permissions.askAsync(Permissions.CAMERA);
-        this.setState({ hasCameraPermission: status === 'granted' });
-      }
-      render() {
-        const { hasCameraPermission } = this.state;
-        if (hasCameraPermission === null) {
-          return <View />;
-        } else if (hasCameraPermission === false) {
-          return <Text>No access to camera</Text>;
-        } else {
-          return (
-            <View style={{ flex: 1 }}>
-              <Camera 
-              ref={cameraref=>{_camera=cameraref}}
-              onTouchStart={this.ScanImage}
-              style={{ flex: 1 }} type={this.state.type}
-              ratio="16:9" useCamera2Api={true} autoFocus="on"
-              >
-                <View
-
-                  style={{
-                    flex: 1,
-                    backgroundColor: 'transparant',
-                  }}>
-                  <TouchableWithoutFeedback 
-                  onPress={this.ScanImage}
-                  style={{
-                    height:"100%",
-                    width:"100%"
-                  }}>
-
-                  </TouchableWithoutFeedback>
-                </View>
-              </Camera>
-            </View>
-          );
-        }
-      }
+  constructor(props) {
+    super(props);
+    this.ScanImage = this.ScanImage.bind(this);
   }
+
+  state = {
+    hasCameraPermission: null,
+    type: Camera.Constants.Type.back
+  };
+
+  ScanImage() {
+    _camera.takePictureAsync().then(r => {
+      Filesystem.readAsStringAsync(r.uri, {
+        encoding: Filesystem.EncodingType.Base64
+      }).then(s => {
+        fetch(ENDPOINT + "animal", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          
+          body: JSON.stringify({
+            Base64: s
+          })
+        }).then(res => {
+          // TODO: CHECK AND RETURN RESULT
+          console.warn(res);
+        }).catch(e=>{
+          // TODO: LET COMPANION EXPLAIN THE ISSUE...
+        })
+      });
+    });
+  }
+  async componentDidMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ hasCameraPermission: status === "granted" });
+  }
+  render() {
+    const { hasCameraPermission } = this.state;
+    if (hasCameraPermission === null) {
+      return <View />;
+    } else if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    } else {
+      return (
+        <View style={{ flex: 1 }}>
+          <Camera
+            ref={cameraref => {
+              _camera = cameraref;
+            }}
+            onTouchStart={this.ScanImage}
+            style={{ flex: 1 }}
+            type={this.state.type}
+            ratio="16:9"
+            useCamera2Api={true}
+            autoFocus="on"
+          >
+            <View
+              style={{
+                flex: 1,
+                backgroundColor: "transparant"
+              }}
+            >
+              <TouchableWithoutFeedback
+                onPress={this.ScanImage}
+                style={{
+                  height: "100%",
+                  width: "100%"
+                }}
+              ></TouchableWithoutFeedback>
+            </View>
+          </Camera>
+        </View>
+      );
+    }
+  }
+}
 
 // #region Stylesheet
 const styles = StyleSheet.create({
-    c_background:{
-      height: ScreenHeight,
-      width: ScreenWidth,
-      position: 'absolute'
-    },
-    contentContainer:{
-        backgroundColor:"#000"
-    }
-  });
- // #endregion
+  c_background: {
+    height: ScreenHeight,
+    width: ScreenWidth,
+    position: "absolute"
+  },
+  contentContainer: {
+    backgroundColor: "#000"
+  }
+});
+// #endregion
