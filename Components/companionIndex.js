f97b5718f7f11f243ccc2509313fd4ea8e0dd480
Bram Robyn <bram.robyn@student.howest.be>
2019-11-29 00:34:09 +0100

    Update companionIndex.js

--- Components/companionIndex.js
+++ Components/companionIndex.js
@@ -30,7 +30,7 @@ export default class Companion extends React.Component {
                     <View style={styles.c_companion_upper_cover}/>
                     <View style={styles.c_companion__cover}>
                         <View style={styles.c_companionContainer}>
-                            <SvgUri height={200} width={200} style={styles.c_companion} source={require('../assets/companion.svg')}/>
+                            <SvgUri height={150} width={150} style={styles.c_companion} source={require('../assets/companion.svg')}/>
                             <View style={styles.c_companion__text_container}>
                                 <Text style={styles.c_companion__title}>Willy: </Text>
                                 <Text style={styles.c_companion__text}>You can see the info about the animal that is currently selected.</Text>
@@ -83,8 +83,8 @@ const styles = StyleSheet.create({
         borderTopRightRadius: 15,
         borderTopLeftRadius: 15,
         position: 'absolute',
-        width: ScreenWidth,
-        height: ScreenHeight/4,
+        width: '100%',
+        height: '30%',
         bottom: -50,
         flex:1,
         flexDirection:'row',
@@ -98,7 +98,7 @@ const styles = StyleSheet.create({
     c_companion__text_container:{
         marginTop: 24,
         marginLeft: 24,
-        width: 250
+        width: '50%'
     },
 
     c_companion__title:{
